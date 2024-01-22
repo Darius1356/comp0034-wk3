2b298f79dcc14824e28738b1876ef012024476ab
Darius <dariusjoshi@hotmail.co.uk>
2024-01-22 13:47:59 +0000

    Implemented get(regions) route

--- paralympics/paralympics.py
+++ paralympics/paralympics.py
@@ -1,5 +1,23 @@
 from flask import current_app as app
+from paralympics.schemas import RegionSchema, EventSchema
+from paralympics import db
+from paralympics.models import Region, Event
 
+# Flask-Marshmallow Schemas
+regions_schema = RegionSchema(many=True)
+region_schema = RegionSchema()
+events_schema = EventSchema(many=True)
+event_schema = EventSchema()
+
+@app.get("/regions")
+def get_regions():
+    """Returns a list of NOC regions and their details in JSON."""
+    # Select all the regions using Flask-SQLAlchemy
+    all_regions = db.session.execute(db.select(Region)).scalars()
+    # Get the data using Marshmallow schema (returns JSON)
+    result = regions_schema.dump(all_regions)
+    # Return the data
+    return result
 
 @app.route('/')
 def hello():
